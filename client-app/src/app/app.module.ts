dc0666c3a50e2db179221c8673bb7437ed24fd0f
Thiago Pires Romanelli Vicente <rm334527@fiap.com.br>
2020-03-30 00:14:14 -0300

    added frontend funcionalities for register, edit, delete and get Lancamentos Financeiros

--- client-app/src/app/app.module.ts
+++ client-app/src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from "@angular/common/http";
 
 import { AppComponent } from './app.component';
+import { LancamentoComponent } from './lancamentos/lancamento/lancamento.component';
+import { LancamentosComponent } from './lancamentos/lancamentos.component';
+import { LancamentoService } from './shared/lancamento.service';
+import { LancamentoListComponent } from './lancamentos/lancamento-list/lancamento-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LancamentoComponent,
+    LancamentosComponent,
+    LancamentoListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
   ],
-  providers: [],
+  providers: [LancamentoService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
